9dfebbba5e7ca5723d22ac77434d0701f5fff9f9
Kirill <kirill.shash@mail.ru>
2019-10-21 17:48:41 +0300

    Final version Range

--- Range/src/ru/academit/shashkov/range/Range.java
+++ Range/src/ru/academit/shashkov/range/Range.java
@@ -4,7 +4,7 @@ public class Range {
     private double from;
     private double to;
 
-    public Range (double from, double to){
+    public Range(double from, double to) {
         this.from = from;
         this.to = to;
     }
@@ -25,12 +25,70 @@ public class Range {
         this.to = to;
     }
 
-    public double getLength(){
+    public double getLength() {
         return this.to - this.from;
     }
 
     public boolean isInside(double number) {
-        return (number <= this.to) && (number >= this.from);
+        return (number <= to) && (number >= from);
     }
 
+    public Range getIntersection(Range range2) {
+        if (from >= range2.to || range2.from >= to) {
+            return null;
+        }
+        if (range2.from >= from && range2.to <= to) {
+            return new Range(range2.from, range2.to);
+        }
+
+        if (range2.from <= from && range2.to > to) {
+            return new Range(from, to);
+        }
+
+        if (from < range2.from && to < range2.to) {
+            return new Range(range2.from, to);
+        }
+
+        return new Range(from, range2.to);
+    }
+
+    public Range[] getUnion(Range range2) {
+        if (from > range2.to || range2.from > to) {
+            return new Range[]{new Range(from, to), new Range(range2.from, range2.to)};
+        }
+
+        if (from < range2.from && to > range2.to) {
+            return new Range[]{new Range(from, to)};
+        }
+
+        if (from >= range2.from && to <= range2.to) {
+            return new Range[]{new Range(range2.from, range2.to)};
+        }
+
+        if (from < range2.from && to <= range2.to) {
+            return new Range[]{new Range(from, range2.to)};
+        }
+
+        return new Range[]{new Range(range2.from, to)};
+    }
+
+    public Range[] getDifference(Range range2) {
+        if (range2.from > to || range2.to < from) {
+            return new Range[]{new Range(from, to)};
+        }
+
+        if (range2.from > from && range2.to < to) {
+            return new Range[]{new Range(from, range2.from), new Range(range2.to, to)};
+        }
+
+        if (range2.from <= from && range2.to >= to) {
+            return new Range[0];
+        }
+
+        if (range2.from > from && range2.to >= to) {
+            return new Range[]{new Range(from, range2.from)};
+        }
+
+        return new Range[]{new Range(range2.to, to)};
+    }
 }
--- Range/src/ru/academit/shashkov/range/main/Main.java
+++ Range/src/ru/academit/shashkov/range/main/Main.java
@@ -1,12 +1,67 @@
 package ru.academit.shashkov.range.main;
+
 import ru.academit.shashkov.range.Range;
+
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Range range1 = new Range(3, 10);
-        Range range2 = new Range(5, 15);
-        double length = range1.getLength();
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println(length + System.lineSeparator() + range2.isInside(7));
+        System.out.println("Начало первого интервала:");
+        double from1 = scanner.nextDouble();
+
+        System.out.println("Конец первого интервала:");
+        double to1 = scanner.nextDouble();
+
+        System.out.println("Начало второго интервала:");
+        double from2 = scanner.nextDouble();
+
+        System.out.println("Конец второго интервала:");
+        double to2 = scanner.nextDouble();
+
+        Range range1 = new Range(from1, to1);
+        Range range2 = new Range(from2, to2);
+
+        System.out.printf("Первый интервал: [%.2f %.2f]%nВторой интервал: [%.2f %.2f]%n", from1, to1, from2, to2);
+
+        System.out.println("Число для проверки вхождения в интервалы:");
+        double number = scanner.nextDouble();
+
+        System.out.printf("Входит ли число в первый интервал - %b%n", range1.isInside(number));
+        System.out.printf("Входит ли число во второй интервал - %b%n", range2.isInside(number));
+
+        double length1 = range1.getLength();
+        double length2 = range2.getLength();
+
+        System.out.printf("Длина первого интервала = %.2f%n", length1);
+        System.out.printf("Длина второго интервала = %.2f%n", length2);
+
+        Range intersectionOfRanges = range1.getIntersection(range2);
+
+        if (intersectionOfRanges == null) {
+            System.out.println("Пересечений интервалов нет.");
+        } else {
+            System.out.printf("Интервал пересечения - [%.2f %.2f]%n", intersectionOfRanges.getFrom(), intersectionOfRanges.getTo());
+        }
+
+        Range[] union = range1.getUnion(range2);
+
+        if (union.length > 1) {
+            System.out.printf("Результатом объединения являются: [%.2f %.2f] и [%.2f %.2f]%n", union[0].getFrom(), union[0].getTo(), union[1].getFrom(), union[1].getTo());
+        } else {
+            System.out.printf("Результатом объединения явлется: [%.2f %.2f]%n", union[0].getFrom(), union[0].getTo());
+        }
+
+        Range[] difference = range1.getDifference(range2);
+
+        if (difference.length > 1) {
+            System.out.printf("Результатом разности являются: [%.2f %.2f] и [%.2f %.2f]%n", difference[0].getFrom(), difference[0].getTo(), difference[1].getFrom(), difference[1].getTo());
+        } else if (difference.length == 0) {
+            System.out.println("Разность данных интервалов - пустой интервал.");
+        } else {
+            System.out.printf("Результатом разности явлется: [%.2f %.2f]%n", difference[0].getFrom(), difference[0].getTo());
+        }
     }
 }
