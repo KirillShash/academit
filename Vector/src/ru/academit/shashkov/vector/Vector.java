f0bf9325eb580178aeb35d5d454ef858352bc342
Kirill <kirill.shash@mail.ru>
2019-10-31 02:19:10 +0300

    Vector на проверку

--- Vector/src/ru/academit/shashkov/vector/Vector.java
+++ Vector/src/ru/academit/shashkov/vector/Vector.java
@@ -10,11 +10,11 @@ public class Vector {
             throw new IllegalArgumentException("Размерность массива должна быть больше 0!");
         }
 
-        this.elements = new double[n];
+        elements = new double[n];
     }
 
     public Vector(Vector vector) {
-        this.elements = vector.elements;
+        elements = Arrays.copyOf(vector.elements, vector.elements.length);
     }
 
     public Vector(double[] arr) {
@@ -22,7 +22,7 @@ public class Vector {
             throw new IllegalArgumentException("Размерность массива должна быть больше 0!");
         }
 
-        this.elements = Arrays.copyOf(arr, arr.length);
+        elements = Arrays.copyOf(arr, arr.length);
     }
 
     public Vector(int n, double[] arr) {
@@ -30,7 +30,83 @@ public class Vector {
             throw new IllegalArgumentException("Размерность массива должна быть больше 0!");
         }
 
-        this.elements = Arrays.copyOf(arr, n);
+        elements = Arrays.copyOf(arr, n);
+    }
+
+    public double getElement(int index) {
+        if ((index > elements.length - 1) || index < 0) {
+            throw new IllegalArgumentException("Вы ввели отсутствующий индекс!");
+        }
+
+        return elements[index];
+    }
+
+    public void setElement(int index, double element) {
+        if ((index > elements.length - 1) || index < 0) {
+            throw new IllegalArgumentException("Вы ввели отсутствующий индекс!");
+        }
+
+        elements[index] = element;
+    }
+
+    public int getSize() {
+        return elements.length;
+    }
+
+    public Vector getSum(Vector vector) {
+        if (elements.length < vector.elements.length) {
+            elements = Arrays.copyOf(elements, vector.elements.length);
+        } else {
+            vector.elements = Arrays.copyOf(vector.elements, elements.length);
+        }
+
+        for (int i = 0; i < elements.length; i++) {
+            elements[i] += vector.elements[i];
+        }
+
+        return this;
+    }
+
+    public Vector getDifference(Vector vector) {
+        if (elements.length < vector.elements.length) {
+            elements = Arrays.copyOf(elements, vector.elements.length);
+        } else {
+            vector.elements = Arrays.copyOf(vector.elements, elements.length);
+        }
+
+        for (int i = 0; i < elements.length; i++) {
+            elements[i] -= vector.elements[i];
+        }
+
+        return this;
+    }
+
+    public Vector getMultiplyByScalar(double scalar) {
+        for (int i = 0; i < elements.length; i++) {
+            elements[i] *= scalar;
+        }
+
+        return this;
+    }
+
+    public Vector getRotate() {
+        for (int i = 0; i < elements.length; i++) {
+            elements[i] *= -1;
+        }
+
+        return this;
+    }
+
+    public double getLength() {
+        double length = 0;
+
+        for (int i = 0; i < elements.length - 1; ++i) {
+            length += Math.pow(elements[i], 2);
+        }
+
+        length = Math.sqrt(length + Math.pow(elements[elements.length - 1], 2));
+
+        return length;
     }
 
     @Override
@@ -38,4 +114,66 @@ public class Vector {
         return Arrays.toString(elements);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || o.getClass() != this.getClass()) {
+            return false;
+        }
+
+        Vector vector = (Vector) o;
+
+        if (this.elements.length != vector.elements.length) {
+            return false;
+        }
+
+        for (int i = 0; i < vector.elements.length; i++) {
+            if (Math.abs(this.elements[i]) != Math.abs(vector.elements[i])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int prime = 37;
+        int hash = 1;
+
+        hash = hash * prime + elements.length;
+        hash = hash * prime + Arrays.hashCode(elements);
+
+        return hash;
+    }
+
+    public static Vector getSum(Vector vector1, Vector vector2) {
+        Vector v = new Vector(vector1);
+
+        v.getSum(vector2);
+
+        return v;
+    }
+
+    public static Vector getDifference(Vector vector1, Vector vector2) {
+        Vector v = new Vector(vector1);
+
+        v.getDifference(vector2);
+
+        return v;
+    }
+
+    public static double getMultiply(Vector vector1, Vector vector2) {
+        int minSize = Math.min(vector1.getSize(), vector2.getSize());
+        double result = 0;
+
+        for (int i = 0; i < minSize; i++) {
+            result += vector1.elements[i] * vector2.elements[i];
+        }
+
+        return result;
+    }
 }
--- Vector/src/ru/academit/shashkov/vector/main/Main.java
+++ Vector/src/ru/academit/shashkov/vector/main/Main.java
@@ -4,13 +4,40 @@ import ru.academit.shashkov.vector.Vector;
 
 public class Main {
     public static void main(String[] args) {
-        double[] x = {1, 2, 3, 4};
-        Vector v = new Vector(x);
-        System.out.println(v);
-        double[] y = {1, 2, 3,4,5,6,7};
-        Vector ve = new Vector(8, y);
-        System.out.println(ve);
-        Vector vector = new Vector(y);
-        System.out.println(vector);
+        Vector vector1 = new Vector(6);
+        Vector vector2 = new Vector(vector1);
+        Vector vector3 = new Vector(new double[]{1, 2, 3, 4, 5});
+        Vector vector4 = new Vector(7, new double[]{1, 2, 3, 4, 5});
+        Vector vector5 = new Vector(new double[]{-3, -1, -5, -7, -4});
+        Vector vector6 = new Vector(new double[]{3, 1, 5, 7, 4});
+        Vector vector7 = new Vector(new double[]{-3, -1, 2, 8});
+
+        System.out.printf("Векторы для теста функций:%n№1 = %s%n№2 = %s%n№3 = %s%n№4 = %s%n№5 = %s%n№6 = %s%n№7 = %s%n", vector1, vector2, vector3, vector4, vector5, vector6, vector7);
+
+        System.out.println("Поиск элемента по индексу 4 из вектора №3: " + vector3.getElement(4));
+
+        vector3.setElement(4, 4.5);
+        System.out.println("Этот же элемент после замены: " + vector3.getElement(4));
+
+        System.out.println("Размерность вектора №4 = " + vector4.getSize());
+
+        System.out.println("Нестатическое сложение вектора №1 и вектора №3 = " + vector1.getSum(vector3));
+
+        System.out.println("Нестатическое вычитание вектора №4 из вектора №3 = " + vector4.getDifference(vector3));
+
+        System.out.println("Разворот вектора № 3 = " + vector3.getRotate());
+
+        System.out.println("Умножение на скаляр 5 вектора №3 = " + vector3.getMultiplyByScalar(5));
+
+        System.out.println("Длина вектора №5 = " + vector5.getLength());
+
+        System.out.println("Равны ли векторы №5 и №6 = " + vector5.equals(vector6));
+        System.out.println("Равны ли векторы №5 и №7 = " + vector5.equals(vector7));
+
+        System.out.println("Статическое сложение вектора №5 и вектора №6 = " + Vector.getSum(vector5, vector6));
+
+        System.out.println("Статическое вычитание вектора №5 из вектора №6 = " + Vector.getDifference(vector5, vector6));
+
+        System.out.println("Скалярное произведение векторов №5 и №6 = " + Vector.getMultiply(vector5, vector6));
     }
 }
